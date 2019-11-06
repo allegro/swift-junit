a8d149180979aabee31164618e0514e10ce79b43
Kamil Borzym <kamil.borzym@gmail.com>
2019-11-06 11:17:27 +0100

    Improve className resolution (#8) (#9)

--- Sources/SwiftTestReporter/Utils.swift
+++ Sources/SwiftTestReporter/Utils.swift
@@ -4,16 +4,13 @@ private func lastChunk(_ phrase: String, separator: Character = ".") -> String {
     return phrase.split(separator: separator).last?.description ?? phrase
 }
 
-/// Simple helper that returns class name for XCTest.
-/// There are some difference beween original XCTest and XCTest in open source and this
-/// helper solves one of the problems - missing property (className).
-/// - Parameter obj: XCTest's instance
-/// - Returns: A normalized name for XCTest's instance
-func getXCTestClassName(_ obj: XCTest) -> String {
-    #if os(Linux)
-    let className = "\(obj.self)"
-    #else
-    let className = obj.className
-    #endif
-    return lastChunk(className)
+extension XCTest {
+    /// Simple helper that returns class name for XCTest.
+    /// There are some difference between original XCTest on different platforms and XCTest in open source and this
+    /// helper solves one of the problems - missing property (className).
+    /// - Returns: A normalized name for XCTest's instance
+    var customClassName : String {
+        let fullClassName = "\(type(of: self))"
+        return fullClassName
+    }
 }
